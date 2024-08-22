19c4f771caa1565618a8590e405f8e8413e32c23
Chrysanthemum Lovelace <chryslovelace@gmail.com>
2024-08-22 17:35:42 +0000

    fix install hook

--- src/EventSubscriber/DomainSearchSubscriber.php
+++ src/EventSubscriber/DomainSearchSubscriber.php
@@ -11,27 +11,27 @@ use Drupal\search_api\Event\QueryPreExecuteEvent;
 class DomainSearchSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents() {
     return [
-      SearchApiEvents::INDEXING_ITEMS => 'indexingItems',
+      // SearchApiEvents::INDEXING_ITEMS => 'indexingItems',
       SearchApiEvents::QUERY_PRE_EXECUTE => 'executingQuery'
     ];
   }
 
+  // commented out because it appears this is happening automatically - needs investigation
   public function indexingItems(IndexingItemsEvent $event) {
-    // return;
-    foreach ($event->getItems() as $item) {
-      $original_object = $item->getOriginalObject(true);
-      $entity = $original_object->getEntity();
-      $domains = DomainAccessManager::getAccessValues($entity);
-      $item->setField('domain_ids', $domains);
-    }
+    // foreach ($event->getItems() as $item) {
+    //   $original_object = $item->getOriginalObject(true);
+    //   $entity = $original_object->getEntity();
+    //   $domains = DomainAccessManager::getAccessValues($entity);
+    //   $field = $item->getField(DomainAccessManager::DOMAIN_ACCESS_FIELD);
+    //   $field->setValues(array_keys($domains));
+    // }
   }
 
   public function executingQuery(QueryPreExecuteEvent $event) {
-    // return;
     $domain_negotiator = \Drupal::service('domain.negotiator');
     $domain_id = $domain_negotiator->getActiveId();
     $query = $event->getQuery();
     $conditions = $query->createAndAddConditionGroup();
-    $conditions->addCondition('domain_ids', $domain_id, 'IN');
+    $conditions->addCondition(DomainAccessManager::DOMAIN_ACCESS_FIELD, $domain_id, 'IN');
   }
 }
